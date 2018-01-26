2135fa92e2e6e66e8d4a05f6e8322b270e3fb678
wang-jinpeng <857245460@qq.com>
2018-01-26 08:57:34 +0800

    zhuye

--- js/index.js
+++ js/index.js
@@ -10,15 +10,41 @@ $("#box").fullpage({
     menu: "#menu",
     slidesNavigation:true,
     afterLoad:function(anchor,index){
+        if (index===1){
+            $(".home_content").css("transform","translateY(0)");
+        }
         if(index===2){
             $(".about_left").css("transform","translateX(0)");
             $(".about_right").css("transform","translateX(0)");
+            $(".about-head").css("transform","translateY(0)");
+        }
+        if (index===3){
+            $(".jineng").css("transform","scale(1);").addClass("xuanzhaun");
+        }
+        if (index===4){
+            $(".bai").css("transform","translateX(0)");
+        }
+        if (index===5){
+            $(".last").css("transform","translateY(0)");
         }
     },
     onLeave:function(index){
+        if (index===1){
+            $(".home_content").css("transform","translateY(-500px)");
+        }
         if(index===2){
             $(".about_left").css("transform","translateX(-1000px)");
             $(".about_right").css("transform","translateX(1000px)");
+            $(".about-head").css("transform","translateY(-1000px)");
+        }
+        if (index===3){
+            $(".jineng").css("transform","scale(0);").removeClass("xuanzhaun");
+        }
+        if (index===4){
+            $(".bai").css("transform","translateX(1500px)");
+        }
+        if (index===5){
+            $(".last").css("transform","translateY(1000px)");
         }
     }
 })
